6096959e1f1bdb19fb7d40e1cfcdfc2f76e24f57
piksa13 <ania.piktas@gmail.com>
2017-01-16 10:45:13 +0000

    ready

---
+++ app/src/main/java/com/example/android/amazingcakes/AmazingCakes.java
@@ -0,0 +1,13 @@
+package com.example.android.amazingcakes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AmazingCakes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amazing_cakes);
+    }
+}
